691bf0ed72a3c4b12d64918d960c3f89335118af
Alexandre Marchioli <alexandresilva.marchioli@gmail.com>
2025-09-22 22:21:22 -0300

    Second Commit - Creation of controller, service, repository (MySQL) and applications

---
+++ src/main/java/com/alexandre/gerenciamentoKravMaga/controller/AlunoController.java
@@ -0,0 +1,73 @@
+package com.alexandre.gerenciamentoKravMaga.controller;
+
+import com.alexandre.gerenciamentoKravMaga.model.Aluno;
+import com.alexandre.gerenciamentoKravMaga.model.Faixa;
+import com.alexandre.gerenciamentoKravMaga.service.AlunoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/alunos")
+public class AlunoController {
+    @Autowired
+    private AlunoService alunoService;
+
+    @GetMapping
+    public String listarAlunos(Model model){
+        model.addAttribute("alunos", alunoService.listarTodos());
+        return "alunos/lista";
+    }
+
+    @GetMapping("/novo")
+    public String mostrarFormularioNovo(Model model){
+        model.addAttribute("alunos", new Aluno());
+        model.addAttribute("faixas", Faixa.values());
+        return "alunos/form";
+    }
+
+    @PostMapping()
+    public String cadastrarAluno(@Valid @ModelAttribute Aluno aluno,
+                                  BindingResult erros,
+                                  Model model,
+                                  RedirectAttributes ra){
+        if (erros.hasErrors()) {
+            model.addAttribute("faixas", Faixa.values());
+            return "alunos/form";
+        }
+        alunoService.salvar(aluno);
+        ra.addFlashAttribute("msgSucesso", "Aluno cadastrado com sucesso!");
+        return "redirect:/alunos";
+    }
+
+    @GetMapping("editar/{id}")
+    public String abrirEdicao(@PathVariable Long id, Model model, RedirectAttributes ra){
+        if(!(alunoService.existsById(id))){
+            ra.addFlashAttribute("msgErro", "Aluno não encontrado!");
+            return "redirect:/alunos";
+        }
+        model.addAttribute("aluno", alunoService.pesquisarPorId(id).get());
+        model.addAttribute("faixas", Faixa.values());
+        return "alunos/form";
+    }
+
+    @GetMapping("deletar/{id}")
+    public String deletarAluno(@PathVariable Long id, RedirectAttributes ra){
+        if (!alunoService.existsById(id)){
+            ra.addFlashAttribute("msgErro", "Aluno não encontrar!");
+        } else {
+            alunoService.deletarPorId(id);
+            ra.addFlashAttribute("msgSucesso", "Aluno excluído com sucesso!")
+        }
+        return "redirect:/alunos";
+    }
+
+
+}
--- src/main/java/com/alexandre/gerenciamentoKravMaga/service/AlunoService.java
+++ src/main/java/com/alexandre/gerenciamentoKravMaga/service/AlunoService.java
@@ -1,4 +1,46 @@
 package com.alexandre.gerenciamentoKravMaga.service;
 
+import com.alexandre.gerenciamentoKravMaga.model.Aluno;
+import com.alexandre.gerenciamentoKravMaga.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AlunoService {
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public List<Aluno> listarTodos(){
+        return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> pesquisarPorId(Long id){
+        return alunoRepository.findById(id);
+    }
+
+    public Optional<Aluno> pesquisarPorCPF(String cpf){
+        return alunoRepository.findByCpf(cpf);
+    }
+
+    public Optional<Aluno> pesquisarPorEmail(String email){
+        return alunoRepository.findByEmailIgnoreCase(email);
+    }
+
+    public List<Aluno> pesquisarPorNome(String nome){
+        return alunoRepository.findByNomeContainingIgnoreCase(nome);
+    }
+
+    public void salvar(Aluno aluno){
+        alunoRepository.save(aluno); //save cria e atualiza, caso já exista id de aluno
+    }
+
+    public void deletarPorId(Long id){
+        alunoRepository.deleteById(id);
+    }
+
+    public boolean existsById(Long id) {
+        return alunoRepository.existsById(id);
+    }
 }
